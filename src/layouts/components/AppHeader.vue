991e903fe091fb2193c6f1703e12d49022be95bc
zehui <zehui@macrocura.com>
2020-06-30 23:30:02 +0800

    [fixbug] logo更换

--- src/layouts/components/AppHeader.vue
+++ src/layouts/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <div class="app-header">
     <div class="main-container flex ai-center h-100p">
       <router-link to="/">
-        <img src="@/assets/images/logo@2x.png" alt="" width="128" />
+        <img src="@/assets/images/logo@4x.png" alt="" width="128" />
       </router-link>
       <div class="navs flex ai-center ml-32">
         <router-link
