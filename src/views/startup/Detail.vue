3187ecbeb5927a46a17a17e8698ef770f8e4322f
erguotou <erguotou525@gmail.com>
2020-08-03 00:26:30 +0800

    开始写startup详情

---
+++ src/views/startup/Detail.vue
@@ -0,0 +1,39 @@
+<script>
+import TabPerference from './details/Perference';
+export default {
+  components: {
+    TabPerference,
+    TabBounty: () => import('./details/Bounty'),
+    TabExchange: () => import('./details/Exchange'),
+    TabIRO: () => import('./details/IRO'),
+    TabVote: () => import('./details/Vote'),
+    TabProposals: () => import('./details/Proposals')
+  },
+  data() {
+    return {
+      selectedTab: 'Perference',
+      tabs: ['Perference', 'Bounty', 'Exchange', 'IRO', 'Vote', 'Proposals']
+    };
+  },
+  render(h) {
+    return (
+      <div class="flex">
+        <a-card title="Menu">
+          <a-tabs vModel={this.selectedTab} tab-position="left">
+            {this.tabs.map(tab => {
+              const Comp = `Tab${tab}`;
+              return (
+                <a-tab-pane key={tab} tab={tab}>
+                  <Comp id={this.$route.params.id} />
+                </a-tab-pane>
+              );
+            })}
+          </a-tabs>
+        </a-card>
+      </div>
+    );
+  }
+};
+</script>
+
+<style></style>
---
+++ src/views/startup/details/Perference.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-card>
+    <div class="flex ai-center">
+      <img :src="startup.logo || defaultLogo" alt="" width="46" height="46" class="no-shrink" />
+      <div class="flex-1 ml-24">
+        <h3 class="mb-0 lh-1 t-bold t-break t-trunc" :title="startup.name">{{ startup.name }}</h3>
+        <p class="mt-16 mb-0 t-break t-trunc-2">{{ startup.mission }}</p>
+      </div>
+      <a-button type="primary" class="ml-auto">123+ Follow</a-button>
+    </div>
+  </a-card>
+</template>
+
+<script>
+export default {
+  props: {
+    // startup id
+    id: String
+  }
+};
+</script>
+
+<style></style>
