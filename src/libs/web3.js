03f8a5763124a0a34e3ce35a92745a2a1a13ef96
superestze <sbura@sina.com>
2020-09-14 23:16:26 +0800

    feat✨  hunter 接收bounty 向平台转账

--- src/libs/web3.js
+++ src/libs/web3.js
@@ -7,11 +7,17 @@ const DEV_STARTUP_RECEIVER = '0xfC938765401224e62AfA26535ed07fb334d3e11c';
 const DEV_SETTING_RECEIVER = '0xFdE6dC94cE94a9b873977DdE385d33FcA99d8eA5';
 // 测试环境下， bounty 账号收款地址
 const DEV_BOUNTY_RECEIVER = '0x52B82f2a4bE2F5861872B0Cf3c76D9447f94C731';
+// 测试环境下， hunter向平台转账的地址
+const DEV_HUNTER_FOR_COMUNION = '0x52B82f2a4bE2F5861872B0Cf3c76D9447f94C731';
 
 // 生产环境， startup 收款账号
 // const PROD_STARTUP_RECEIVER = '0x50C972636DC3cc2DE457517Ab62279c6eF3050bA';
 // 生产环境下 setting 收款账号地址
 // const PROD_SETTING_RECEIVER = '0x50C972636DC3cc2DE457517Ab62279c6eF3050bA';
+// 生产环境下， bounty 收款地址
+// TODO
+// 生产环境下， hunter 转给平台的手段地址
+// TODO
 
 // comunion new start up接收方钱包地址
 export const COMUNION_RECEIVER_STARTUP_ACCOUNT = DEV_STARTUP_RECEIVER;
@@ -19,6 +25,8 @@ export const COMUNION_RECEIVER_STARTUP_ACCOUNT = DEV_STARTUP_RECEIVER;
 export const COMMUNION_SETTING_RECEIVE_ACCOUNT = DEV_SETTING_RECEIVER;
 // comunion bounty 接收方钱包地址
 export const COMUNION_BOUNTY_RECEIVE_ACCOUNT = DEV_BOUNTY_RECEIVER;
+// hunter 向 comunion 转账的地址
+export const COMUNION_RECEIVE_HUNTER_TRANSFER = DEV_HUNTER_FOR_COMUNION;
 
 // web3示例
 export let web3;
