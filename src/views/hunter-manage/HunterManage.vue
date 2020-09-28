710f34948860dc6d0fc58843573d537048d0b9be
travelclover <travelclover@163.com>
2020-09-28 22:26:49 +0800

    fix: 🐛 #cn-93修改样式

--- src/views/hunter-manage/HunterManage.vue
+++ src/views/hunter-manage/HunterManage.vue
@@ -1,21 +1,23 @@
 <template>
-  <a-card id="hunter-manage" :bordered="false" style="margin-bottom: 24px;">
+  <div class="hunter-manage">
     <h1 class="title">Hunter Management</h1>
-    <section class="content">
-      <a-tabs>
-        <a-tab-pane key="1" tab="Hunter">
-          <Hunter />
-        </a-tab-pane>
-        <a-tab-pane key="2" tab="Bounty">
-          <!-- 我接到的bounty -->
-          <Bounty />
-        </a-tab-pane>
-        <a-tab-pane key="3" tab="Proposal" disabled>
-          <!--  TODO-->
-        </a-tab-pane>
-      </a-tabs>
-    </section>
-  </a-card>
+    <div class="content mb-24">
+      <div class="tabs flex ai-center jc-around">
+        <div
+          class="tab flex ai-center jc-center f-18 t-bold"
+          :class="{ active: activeTab === tab.name }"
+          v-for="tab in tabs"
+          :key="tab.name"
+          @click="tabOnChange(tab.name)"
+        >
+          {{ tab.name }}
+        </div>
+      </div>
+
+      <Hunter v-if="activeTab === 'Hunter'" />
+      <Bounty v-else-if="activeTab === 'Bounty'" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -25,30 +27,75 @@ export default {
   name: 'hunter-manage',
   components: { Hunter, Bounty },
   data() {
-    return {};
+    return {
+      activeTab: 'Hunter',
+      tabs: [{ name: 'Hunter' }, { name: 'Bounty' }, { name: 'Proposal' }]
+    };
   },
   computed: {},
-  methods: {},
+  methods: {
+    // tab改变
+    tabOnChange(tab) {
+      this.activeTab = tab;
+    }
+  },
   mounted() {}
 };
 </script>
 
 <style scoped lang="less">
-#hunter-manage {
+.hunter-manage {
   .title {
     font-size: 24px;
+    line-height: 24px;
     font-family: Microsoft YaHei;
     font-weight: bold;
     color: rgba(0, 0, 0, 1);
-    margin-bottom: 24px;
+    margin: 30px 0 48px;
     text-align: center;
   }
 
   .content {
-    margin: 0 auto;
-    width: 60%;
-    max-width: 640px;
-    min-width: 240px;
+    background-color: #fff;
+    box-shadow: 0px 2px 4px 0px rgba(6, 0, 1, 0.04);
+    border-radius: 4px;
+
+    .tabs {
+      height: 70px;
+      position: relative;
+      padding: 0 30px;
+
+      &:after {
+        content: '';
+        position: absolute;
+        left: 30px;
+        right: 30px;
+        bottom: 0;
+        border-bottom: 1px solid #bfbfbf;
+      }
+      .tab {
+        color: #000;
+        cursor: pointer;
+        height: 100%;
+        position: relative;
+        padding: 0 32px;
+
+        &:hover,
+        &.active {
+          &:after {
+            content: '';
+            position: absolute;
+            height: 5px;
+            left: 0;
+            right: 0;
+            bottom: -2px;
+            border-radius: 3px;
+            background-color: #6170ff;
+            z-index: 1;
+          }
+        }
+      }
+    }
   }
 }
 </style>
