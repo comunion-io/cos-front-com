9db641a5d80cab2d14eda9605cfce834ed72671e
erguotou <erguotou525@gmail.com>
2020-11-10 15:52:33 +0800

    chore: 🔧 y2s版本升级

--- src/services/yapi.services.ts
+++ src/services/yapi.services.ts
@@ -1,36 +1,36 @@
-import { RequestAdapter, ServiceKeys, ServiceReturn } from './yapi.api';
-import { apis } from './yapi.apis';
+import { RequestAdapter, ServiceKeys, ServiceReturn } from './yapi.api'
+import { apis } from './yapi.apis'
 
 export function createServices(createFunc: RequestAdapter): ServiceReturn {
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  const ret = {} as ServiceReturn;
-  let key: ServiceKeys;
+  const ret = {} as ServiceReturn
+  let key: ServiceKeys
   // eslint-disable-next-line guard-for-in
   for (key in apis) {
-    const api = apis[key];
+    const api = apis[key]
     // @ts-ignore
     ret[key] = (payload: { [key: string]: any }, extraParams?: any) => {
-      let url = api.u;
-      const body = { ...payload };
+      let url = api.u
+      const body = { ...payload }
       // params
       if (api.p?.length) {
         api.p.forEach(paramKey => {
-          delete body[paramKey];
-          url = url.replace(new RegExp(`:${paramKey}|{${paramKey}}`, 'g'), payload[paramKey]);
-        });
+          delete body[paramKey]
+          url = url.replace(new RegExp(`:${paramKey}|{${paramKey}}`, 'g'), payload[paramKey])
+        })
       }
       // query
-      const query: { [key: string]: any } = {};
+      const query: { [key: string]: any } = {}
       if (api.q?.length) {
         api.q.forEach(queryKey => {
           if (queryKey in payload) {
-            delete body[queryKey];
-            query[queryKey] = payload[queryKey];
+            delete body[queryKey]
+            query[queryKey] = payload[queryKey]
           }
-        });
+        })
       }
-      return createFunc(url, api.m, query, body, extraParams, api.d > 0);
-    };
+      return createFunc(url, api.m, query, body, extraParams, api.d > 0)
+    }
   }
-  return ret as ServiceReturn;
+  return ret as ServiceReturn
 }
