920af79a4f39edcd37335e5cae2b9bc637d3b00d
superestze <sbura@sina.com>
2020-11-17 23:03:06 +0800

    fix: 1. 增加tslint.json,2. 注释旧的bounty.service

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,6 @@ export * from './common.service';
 export * from './user.services';
 export * from './startup.services';
 export * from './setting.services';
-export * from './bounty.service';
+// export * from './bounty.service';
 export * from './hunter.service';
 export { services as default } from './service';
