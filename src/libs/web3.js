efdcd8b7cb3eadbe364649149373f71b29ce8c42
superestze <sbura@sina.com>
2020-11-22 13:39:54 +0800

    feat: us#52 【前端】项目配置化，各种地址、钱包地址抽取1

--- src/libs/web3.js
+++ src/libs/web3.js
@@ -1,32 +1,14 @@
 import Web3 from 'web3';
 import { Modal } from 'ant-design-vue';
 
-// 测试环境下， startup 收款账号
-const DEV_STARTUP_RECEIVER = '0xfC938765401224e62AfA26535ed07fb334d3e11c';
-// 测试环境下， setting 账号收款账号
-const DEV_SETTING_RECEIVER = '0xFdE6dC94cE94a9b873977DdE385d33FcA99d8eA5';
-// 测试环境下， bounty 账号收款地址
-const DEV_BOUNTY_RECEIVER = '0x52B82f2a4bE2F5861872B0Cf3c76D9447f94C731';
-// 测试环境下， hunter向平台转账的地址
-const DEV_HUNTER_FOR_COMUNION = '0x69EAB953C4a286Bb2216153EaA842d4c3e651aa2';
-
-// 生产环境， startup 收款账号
-// const PROD_STARTUP_RECEIVER = '0x50C972636DC3cc2DE457517Ab62279c6eF3050bA';
-// 生产环境下 setting 收款账号地址
-// const PROD_SETTING_RECEIVER = '0x50C972636DC3cc2DE457517Ab62279c6eF3050bA';
-// 生产环境下， bounty 收款地址
-// TODO
-// 生产环境下， hunter 转给平台的手段地址
-// TODO
-
 // comunion new start up接收方钱包地址
-export const COMUNION_RECEIVER_STARTUP_ACCOUNT = DEV_STARTUP_RECEIVER;
+export const COMUNION_RECEIVER_STARTUP_ACCOUNT = process.env.VUE_APP_STARTUP_RECEIVER;
 // comunion setting 接收方钱包地址
-export const COMMUNION_SETTING_RECEIVE_ACCOUNT = DEV_SETTING_RECEIVER;
+export const COMMUNION_SETTING_RECEIVE_ACCOUNT = process.env.VUE_APP_SETTING_RECEIVER;
 // comunion bounty 接收方钱包地址
-export const COMUNION_BOUNTY_RECEIVE_ACCOUNT = DEV_BOUNTY_RECEIVER;
+export const COMUNION_BOUNTY_RECEIVE_ACCOUNT = process.env.VUE_APP_BOUNTY_RECEIVER;
 // hunter 向 comunion 转账的地址
-export const COMUNION_RECEIVE_HUNTER_TRANSFER = DEV_HUNTER_FOR_COMUNION;
+export const COMUNION_RECEIVE_HUNTER_TRANSFER = process.env.VUE_APP_HUNTER_FOR_COMUNION;
 
 // web3示例
 export let web3;
