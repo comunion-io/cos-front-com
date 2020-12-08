2efd6a78df4d4f75bedf8744a2a3843bb5887bf4
rebirthO <benyue.liu@jimeng.io>
2020-12-09 00:17:50 +0800

    add card component

---
+++ src/utils/fmt.js
@@ -0,0 +1,15 @@
+import { isNumber } from './utils';
+
+/**
+ * 格式化数字为字符串格式，如：3500 -> "3,500"
+ * @param {Number} num 需要格式化的数字
+ */
+export function fmtNumber(num) {
+  num = +num;
+  if (!isNumber(num) || isNaN(num)) {
+    return num;
+  }
+
+  // TODO: 根据不同语言区域进行区分格式化数字
+  return new Intl.NumberFormat().format(num);
+}
