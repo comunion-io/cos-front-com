822166010623b23be9d4dea0d52f3d8563a36923
erguotou <erguotou525@gmail.com>
2021-01-20 23:56:39 +0800

    feat: ✨ 接入disco详情组件

--- src/components/disco-detail/index.vue
+++ src/components/disco-detail/index.vue
@@ -11,8 +11,9 @@ export default {
   },
   data() {
     return {
+      status: null,
       detail: {}
-    }
+    };
   },
   computed: {
     modules() {
@@ -82,10 +83,17 @@ export default {
   watch: {
     id: {
       immediate: true,
-      handler(v) {
-        const { error, data } = await services['cores@disco-startup-获取']({ startupId: v })
+      async handler(v) {
+        const { error, data } = await services['cores@startup-disco和swap状态']({ startupId: v });
         if (!error) {
-          this.detail = data
+          this.status = data.discoState;
+          // -1 未开启，0 创建中，1 等待开启（创建成功），2 创建失败，3 开启中，4 等待募资开始（数据库为4，时间未开始），5 募资成功，6 募资失败，7 募资中（数据库为4，时间进行中），8 募资结束（数据库为4，时间已结束）
+          if (this.status >= 1) {
+            const res = await services['cores@disco-startup-获取']({ startupId: v });
+            if (!res.error) {
+              this.detail = res.data;
+            }
+          }
         }
       }
     }
@@ -96,27 +104,34 @@ export default {
     }
   },
   render(h) {
-    return (
-      <div class="disco-container">
-        {this.modules.map(module => {
-          const { fields } = module;
-          return (
-            <div class="mb-32">
-              <Descriptions columns={fields} labelWidth={300} dataSource={this.detail} />
-            </div>
-          );
-        })}
-        {/** 暂时注释掉，在Sprint04的时候返注
+    switch (this.status) {
+      case null:
+        return <a-spin />;
+      case 0:
+        return <span>未开启</span>;
+      default:
+        return (
+          <div class="disco-container">
+            {this.modules.map(module => {
+              const { fields } = module;
+              return (
+                <div class="mb-32">
+                  <Descriptions columns={fields} labelWidth={300} dataSource={this.detail} />
+                </div>
+              );
+            })}
+            {/** 暂时注释掉，在Sprint04的时候返注
            <a href="#" class="disco-desc">
           What is DISCO
         </a>
         */}
 
-        <a-button class="invest-btn" onClick={this.invest} type="primary" block>
-          Invest
-        </a-button>
-      </div>
-    );
+            <a-button class="invest-btn" onClick={this.invest} type="primary" block>
+              Invest
+            </a-button>
+          </div>
+        );
+    }
   }
 };
 </script>
---
+++ src/views/startup/details/Disco.vue
@@ -0,0 +1,14 @@
+<template>
+  <disco-detail :id="$route.params.id"> </disco-detail>
+</template>
+
+<script>
+import DiscoDetail from '@/components/disco-detail';
+export default {
+  components: {
+    DiscoDetail
+  }
+};
+</script>
+
+<style></style>
