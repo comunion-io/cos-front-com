82d9f1b9ed8ea4854c18d567f8d7962923131486
erguotou <erguotou525@gmail.com>
2021-01-24 19:33:41 +0800

    feat: ✨ 完成disco详情连调

--- src/components/disco-detail/index.vue
+++ src/components/disco-detail/index.vue
@@ -1,13 +1,26 @@
 <script>
 import Descriptions from '@/components/display/Descriptions';
 import services from '@/services';
+import moment from 'moment';
+import Investors from './investors';
+
+// 0 默认状态，1 等待开始，2 进行中，3 失败，4 成功
+const stateBadgeStatusMap = {
+  0: 'default',
+  1: 'warning',
+  2: 'processing',
+  3: 'error',
+  4: 'success'
+};
+
 export default {
   name: 'DiscoDetail',
   components: {
     Descriptions
   },
   props: {
-    id: String
+    id: String,
+    noBtn: Boolean
   },
   data() {
     return {
@@ -22,58 +35,93 @@ export default {
           fields: [
             {
               label: 'Fund-Raising Contract Addrrss',
-              value: 'txId',
-              copyable: 'true'
+              value: 'fundRaisingAddr',
+              copyable: true
             },
             {
               label: 'Start-Up Wallet Address',
               value: 'walletAddr',
-              copyable: 'true'
+              copyable: true
             },
             {
               label: 'Token Contract',
-              value: 'tokenContract',
-              copyable: 'true'
+              value: 'tokenAddr',
+              copyable: true
             },
             {
               label: 'Description',
-              value: 'description'
+              value: 'description',
+              render: v => {
+                return (
+                  <a href={v} target="_black">
+                    {v}
+                  </a>
+                );
+              }
             },
             {
               label: 'Fund-Raising Time',
-              value: 'fundRaisingTime'
-            },
-            {
-              label: 'Sharte Token',
-              value: 'shareToken'
-            },
-            {
-              label: 'Minimum Fundraising ETH',
-              value: 'minFundRaising'
+              value: 'fundRaisingStartedAt',
+              render: (v, record) => {
+                return v ? (
+                  <span>
+                    {moment(v).format('YYYY-MM-DD')} ~{' '}
+                    {moment(record.fundRaisingEndedAt).format('YYYY-MM-DD')}
+                  </span>
+                ) : null;
+              }
             },
             {
               label: 'Investment Reward',
-              value: 'investmentReward'
+              value: 'investmentReward',
+              render: v => `${v} %`
             },
             {
-              label: 'Add Liquidity Token',
-              value: 'addLiquidityPool'
+              label: '* Reward Decline Rate (Day)',
+              value: 'rewardDeclineRate',
+              render: v => `${v} %`
+            },
+            {
+              label: 'Sharte Token',
+              value: 'shareToken',
+              render: v => `${v} Token`
+            },
+            {
+              label: 'Fund-Raising ETH (min)',
+              value: 'minFundRaising',
+              render: v => `${v} ETH`
+            },
+            {
+              label: 'Add Liquidity Pool',
+              value: 'addLiquidityPool',
+              render: v => `${v} %`
             },
             {
               label: 'Total Deposit Token',
-              value: 'totalDepositToken'
+              value: 'totalDepositToken',
+              render: v => `${v} Token`
             },
             {
               label: 'Status',
-              value: 'status'
+              value: 'state',
+              render: v => {
+                return (
+                  <span class="flex-inline ai-center">
+                    <a-badge status={stateBadgeStatusMap[v]} />
+                    {this.getStatusNode(v)}
+                  </span>
+                );
+              }
             },
             {
-              label: 'TokenRaised Until Now',
-              value: 'tokenRaised'
+              label: 'Raised Until Now',
+              value: 'tokenRaised',
+              render: v => `${v} ETH`
             },
             {
               label: 'Investors',
-              value: 'investor'
+              value: 'investors',
+              render: () => <Investors id={this.id} onData={this.updateInvestorsHandler} />
             }
           ]
         }
@@ -88,9 +136,10 @@ export default {
         if (!error) {
           this.status = data.discoState;
           // -1 未开启，0 创建中，1 等待开启（创建成功），2 创建失败，3 开启中，4 等待募资开始（数据库为4，时间未开始），5 募资成功，6 募资失败，7 募资中（数据库为4，时间进行中），8 募资结束（数据库为4，时间已结束）
-          if (this.status >= 1) {
+          if (this.status >= 0) {
             const res = await services['cores@disco-startup-获取']({ startupId: v });
             if (!res.error) {
+              // detail里的state 0 默认状态，1 等待开始，2 进行中，3 失败，4 成功
               this.detail = res.data;
             }
           }
@@ -99,16 +148,55 @@ export default {
     }
   },
   methods: {
-    invest() {
+    getStatusNode(status) {
+      switch (status) {
+        case 1:
+          return <span class="t-primary">Waitting for start</span>;
+        case 2:
+          return <span class="t-primary">In Progress</span>;
+        case 3:
+          return (
+            <span class="t-grey">
+              End <span class="t-error">（Failed）</span>
+            </span>
+          );
+        case 4:
+          return (
+            <span class="t-grey">
+              End <span class="t-primary">（Succeed）</span>
+            </span>
+          );
+        default:
+          return null;
+      }
+    },
+    updateInvestorsHandler(totalETH) {
+      this.$set(this.detail, 'tokenRaised', totalETH);
+    },
+    doInvest() {
       console.log('invest');
     }
   },
   render(h) {
     switch (this.status) {
       case null:
-        return <a-spin />;
-      case 0:
-        return <span>未开启</span>;
+        return (
+          <div class="flex ai-center jc-center py-32">
+            <a-spin />
+          </div>
+        );
+      case -1:
+        return (
+          <div class="not-enabled flex ai-center jc-center">
+            <a-icon
+              type="exclamation-circle"
+              theme="filled"
+              class="mr-12 f-20"
+              style={{ color: '#FAAD14' }}
+            />
+            DISCO does not be enabled.
+          </div>
+        );
       default:
         return (
           <div class="disco-container">
@@ -120,19 +208,50 @@ export default {
                 </div>
               );
             })}
-            {/** 暂时注释掉，在Sprint04的时候返注
-           <a href="#" class="disco-desc">
-          What is DISCO
-        </a>
-        */}
+            <a
+              href="https://bbs.comunion.io/d/287-what-is-the-disco"
+              href="_blank"
+              class="disco-help-link"
+            >
+              What is DISCO
+            </a>
 
-            <a-button class="invest-btn" onClick={this.invest} type="primary" block>
-              Invest
-            </a-button>
+            {!this.noBtn && (
+              <a-button
+                class="invest-btn"
+                disabled={status !== 3}
+                onClick={this.invest}
+                type="primary"
+                size="large"
+                block
+                onClick={this.doInvest}
+              >
+                Invest
+              </a-button>
+            )}
+            {this.status === 5 && this.totalETH && (
+              <div className="mt-16 t-grey">
+                Initial Price Find:{' '}
+                <span class="t-primary">
+                  {Math.floor((this.detail.totalDepositToken / this.totalETH) * 1000) / 1000} Token
+                  per ETH
+                </span>
+              </div>
+            )}
           </div>
         );
     }
   }
 };
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.not-enabled {
+  height: 162px;
+  background: #fef6e9;
+}
+.disco-help-link {
+  display: inline-block;
+  margin-bottom: 52px;
+  text-decoration: underline;
+}
+</style>
--- src/views/startup-management/tabs/DISCOSwap/DISCODetail.vue
+++ src/views/startup-management/tabs/DISCOSwap/DISCODetail.vue
@@ -1,122 +1,14 @@
 <template>
-  <div class="disco-detail">
-    <Descriptions :columns="columns" :dataSource="disco" />
-  </div>
+  <disco-detail :id="$route.params.id" no-btn> </disco-detail>
 </template>
 
 <script>
-import services from '@/services';
-import Descriptions from '@/components/display/Descriptions';
+import DiscoDetail from '@/components/disco-detail';
 export default {
-  data() {
-    return {
-      disco: null
-    };
-  },
   components: {
-    Descriptions
-  },
-  computed: {
-    columns() {
-      let columns = [
-        {
-          label: 'Fund-Raising Contract Address',
-          value: 'walletAddr',
-          copyable: true
-        },
-        {
-          label: 'Start-Up Wallet Address',
-          value: 'walletAddr',
-          copyable: true
-        },
-        {
-          label: 'Token Contract',
-          value: 'tokenContract',
-          copyable: true
-        },
-        {
-          label: 'Description',
-          value: 'description',
-          render: v => (
-            <a href={v} target="_black">
-              {v}
-            </a>
-          )
-        },
-        {
-          label: 'Fund-Raising Time',
-          value: 'fundRaisingTime'
-        },
-        {
-          label: 'Investment Reward',
-          value: 'investmentReward',
-          suffix: '%'
-        },
-        {
-          label: 'Reward Decline Rate (Day)',
-          value: 'rewardDeclineRate',
-          suffix: '%'
-        },
-        {
-          label: 'Share Token',
-          value: 'shareToken',
-          suffix: 'Token'
-        },
-        {
-          label: 'Fund-Raising ETH (min)',
-          value: 'minFundRaising',
-          suffix: 'ETH'
-        },
-        {
-          label: 'Add Liquidity Pool',
-          value: 'addLiquidityPool',
-          suffix: '%'
-        },
-        {
-          label: 'Total Deposit Token',
-          value: 'totalDepositToken',
-          suffix: 'Token'
-        },
-        {
-          label: 'Status',
-          value: 'state',
-          render: value => {
-            return value;
-          }
-        },
-        {
-          label: 'Raised Until Now',
-          value: 'raisedUntilNow',
-          suffix: 'ETH'
-        },
-        {
-          label: 'Investors',
-          value: 'investors'
-        }
-      ];
-      return columns;
-    }
-  },
-  mounted() {
-    // 获取disco信息
-    this.getDisco();
-  },
-  methods: {
-    // 获取disco信息
-    async getDisco() {
-      let { error, data } = await services['cores@disco-startup-获取']({
-        startupId: this.$route.params.id
-      });
-      if (!error) {
-        this.disco = data;
-      }
-    }
+    DiscoDetail
   }
 };
 </script>
 
-<style lang="less" scoped>
-.disco-detail {
-  background-color: #fff;
-}
-</style>
+<style></style>
