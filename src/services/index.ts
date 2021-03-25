d3f9c695c997f430e2a6e30c0264827fcc65167d
erguotou <erguotou525@gmail.com>
2021-03-25 11:48:52 +0800

    fix: 🐛 修复创建bounty时没有获取到startup的问题

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,2 @@
 export * from './request.js';
-export * from './common.service';
-export * from './setting.services';
-export * from './hunter.service';
 export { services as default } from './service';
