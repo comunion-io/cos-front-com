b991d9f522c51c348a68b362da9b7b695194395a
erguotou <erguotou525@gmail.com>
2021-04-17 13:17:18 +0800

    feat: ✨ 修改hunter management页面显示样式

--- src/views/hunter-manage/HunterManage.vue
+++ src/views/hunter-manage/HunterManage.vue
@@ -1,101 +1,53 @@
-<template>
-  <div class="hunter-manage">
-    <h1 class="title">Hunter Management</h1>
-    <div class="content mb-24">
-      <div class="tabs flex ai-center jc-around">
-        <div
-          class="tab flex ai-center jc-center f-18 t-bold"
-          :class="{ active: activeTab === tab.name }"
-          v-for="tab in tabs"
-          :key="tab.name"
-          @click="tabOnChange(tab.name)"
-        >
-          {{ tab.name }}
-        </div>
-      </div>
-
-      <Hunter v-if="activeTab === 'Hunter'" />
-      <Bounty v-else-if="activeTab === 'Bounty'" />
-    </div>
-  </div>
-</template>
-
 <script>
-import Bounty from '@/views/hunter-manage/components/Bounty';
-import Hunter from '@/views/hunter-manage/components/Hunter';
+import TabHunter from './components/Hunter';
 export default {
   name: 'hunter-manage',
-  components: { Hunter, Bounty },
+  components: {
+    TabHunter,
+    TabBounty: () => import('./components/Bounty'),
+    TabProposal: () => import('./components/Proposal')
+  },
   data() {
     return {
       activeTab: 'Hunter',
-      tabs: [{ name: 'Hunter' }, { name: 'Bounty' }, { name: 'Proposal' }]
+      tabs: ['Hunter', 'Bounty', 'Proposal']
     };
   },
-  computed: {},
-  methods: {
-    // tab改变
-    tabOnChange(tab) {
-      this.activeTab = tab;
-    }
-  },
-  mounted() {}
+  render(h) {
+    const TabComponent = `Tab${this.activeTab}`;
+    return (
+      <div class="flex" style="padding: 16px 50px">
+        <a-card title="Hunter Management" class="mr-20 hunter-menus">
+          <a-tabs vModel={this.activeTab} tab-position="left">
+            {this.tabs.map(tab => (
+              <a-tab-pane key={tab} tab={tab} />
+            ))}
+          </a-tabs>
+        </a-card>
+        <a-card class="flex-1">
+          <TabComponent />
+        </a-card>
+      </div>
+    );
+  }
 };
 </script>
 
 <style scoped lang="less">
-.hunter-manage {
-  .title {
-    font-size: 24px;
-    line-height: 24px;
-    font-family: Microsoft YaHei;
-    font-weight: bold;
-    color: rgba(0, 0, 0, 1);
-    margin: 30px 0 48px;
-    text-align: center;
+.hunter-menus {
+  flex-shrink: 0;
+  width: 220px;
+  align-self: flex-start;
+  /deep/ .ant-tabs-left-bar {
+    border-right: none;
   }
-
-  .content {
-    background-color: #fff;
-    box-shadow: 0px 2px 4px 0px rgba(6, 0, 1, 0.04);
-    border-radius: 4px;
-
-    .tabs {
-      height: 70px;
-      position: relative;
-      padding: 0 30px;
-
-      &:after {
-        content: '';
-        position: absolute;
-        left: 30px;
-        right: 30px;
-        bottom: 0;
-        border-bottom: 1px solid #bfbfbf;
-      }
-      .tab {
-        color: #000;
-        cursor: pointer;
-        height: 100%;
-        position: relative;
-        padding: 0 32px;
-
-        &:hover,
-        &.active {
-          &:after {
-            content: '';
-            position: absolute;
-            height: 5px;
-            left: 0;
-            right: 0;
-            bottom: -2px;
-            border-radius: 3px;
-            background-color: #6170ff;
-            z-index: 1;
-          }
-        }
-      }
-    }
+  /deep/ .ant-tabs-tab {
+    margin-bottom: 14px;
+    padding-left: 6px;
+    text-align: left;
+  }
+  /deep/ .ant-tabs-ink-bar {
+    display: none !important;
   }
 }
 </style>
